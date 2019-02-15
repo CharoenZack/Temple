0c4db0e846e0b37f7c3ce880f0d1e9961aa24914
Sukit Prachprayoon <sukit@kkumail.com>
2019-02-15 17:11:25 +0700

    Initial courses module

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoursesComponent } from './courses.component';
+import { CourseResgisterComponent } from './course-resgister/course-resgister.component';
+import { CourseFormComponent } from './course-form/course-form.component';
+import { CourseEditComponent } from './course-edit/course-edit.component';
+import { CourseCreateComponent } from './course-create/course-create.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    CoursesComponent,
+    CourseResgisterComponent,
+    CourseFormComponent,
+    CourseEditComponent,
+    CourseCreateComponent,
+    CoursesListComponent,
+    CourseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoursesModule { }
