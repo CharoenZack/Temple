60c1bb03df432ef0c886130fa7a9c8a7c4771ab5
Babybew <waroon1515@gmail.com>
2019-02-18 10:43:17 +0700

    edit login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  @Input() formTitle:string
+  @Output() data =new EventEmitter<Object>()
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.createForm()
   }
 
+  onSubmit(e){
+    e.preventDefault()
+    const email = this.form.get('email').value
+    const password = this.form.get('password').value
+    if(this.form.valid){
+      this.data.emit({email,password})
+    }
+  }
+
+  private createForm() {
+    this.form = this.formBuilder.group({
+      "email": ['', [Validators.required, Validators.email]],
+      "password":['',Validators.required],
+    })
+  }
 }
