41be1b1ca60e5b0b09343b61816487b9c4860c34
Sukit Prachprayoon <sukit@kkumail.com>
2019-02-18 09:54:10 +0700

    +shared in auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from "../shared/shared.module";
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,7 +12,7 @@ import { AuthRoutingModule } from "./auth-routing.module";
     RegisterComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     AuthRoutingModule
   ],
   exports:[
