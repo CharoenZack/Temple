50a631abe254564c309dcd2563fd5bb93c34df1e
Sukit Prachprayoon <sukit@kkumail.com>
2019-02-20 08:14:36 +0700

    fontawsome

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,24 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReactiveFormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {ProfileFormComponent} from './component/profile-form/profile-form.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ButtonModule} from 'primeng/button';
-import {RadioButtonModule} from 'primeng/radiobutton'
-import {DropdownModule} from 'primeng/dropdown';
-import {TitleNameService } from './service/title-name.service';
-import {CardModule} from 'primeng/card';
-import {InputTextModule} from 'primeng/inputtext';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import {InputMaskModule} from 'primeng/inputmask';
-import {CalendarModule} from 'primeng/calendar';
-import {ToastModule} from 'primeng/toast';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProfileFormComponent } from './component/profile-form/profile-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton'
+import { DropdownModule } from 'primeng/dropdown';
+import { TitleNameService } from './service/title-name.service';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/components/common/messageservice';
 import { SidebarModule } from 'primeng/sidebar';
 import { PasswordModule } from 'primeng/password';
-import {TableModule} from 'primeng/table';
-import {StepsModule} from 'primeng/steps';
+import { TableModule } from 'primeng/table';
+import { StepsModule } from 'primeng/steps';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -70,7 +70,10 @@ import { RegisterInputComponent } from './component/profile-form/register-input/
     StepsModule
 
   ],
-  providers: [TitleNameService,MessageService,ProfileFormService]
+  providers: [TitleNameService, MessageService, ProfileFormService]
 })
 export class SharedModule {
+  constructor() {
+    library.add(faUser, faBars, faBookOpen, faArchive, faLock, faCalendarAlt);
+  }
 }
