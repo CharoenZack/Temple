137040ab467c67a4710fb4912e2e68a38e089347
Sukit Prachprayoon <sukit@kkumail.com>
2019-03-19 11:02:38 +0700

    ไม่รู้ลืมแล้ว

--- src/app/content/personal-info/personal-info.component.ts
+++ src/app/content/personal-info/personal-info.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BreadcrumbService} from '../../shared/service/breadcrumb.service';
 
 @Component({
-  selector: 'app-personal-info',
-  templateUrl: './personal-info.component.html',
-  styleUrls: ['./personal-info.component.css']
+    selector: 'app-personal-info',
+    templateUrl: './personal-info.component.html',
+    styleUrls: ['./personal-info.component.css']
 })
 export class PersonalInfoComponent implements OnInit {
-  public title = 'ข้อมูลส่วนตัว'
-  public personalId :String;
+    public title = 'ข้อมูลส่วนตัว';
+    public personalId: String;
 
-  constructor(
-    private router:Router,
-    private route: ActivatedRoute
-  ) { }
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private breadCrumbService: BreadcrumbService
+    ) {
+    }
 
-  ngOnInit() {
-    this.personalId = this.route.snapshot.paramMap.get('id');
-  }
+    ngOnInit() {
+        this.personalId = this.route.snapshot.paramMap.get('id');
+        this.breadCrumbService.setPath([
+            {label: 'Profile : ข้อมูลส่วนตัว'},
+        ]);
+    }
 
-  editPersonal(){
-    this.router.navigateByUrl(`/profile/${this.personalId}/edit`);
-  }
+    editPersonal() {
+        this.router.navigateByUrl(`/profile/${this.personalId}/edit`);
+    }
 
 }
