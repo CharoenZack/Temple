e4ecc87ced6ef16daae4a2cfd5bd82cd5f9a3e54
YO <panumas.makaew@gmail.com>
2019-03-20 09:38:56 +0700

    เพิ่ม สิทธิเมนู

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MenuItem} from 'primeng/api';
 import {BreadcrumbService} from '../../shared/service/breadcrumb.service';
+import { AuthService } from 'src/app/shared/service/auth.service';
 
 @Component({
     selector: 'app-home',
@@ -10,15 +11,28 @@ import {BreadcrumbService} from '../../shared/service/breadcrumb.service';
 export class HomeComponent implements OnInit {
 
     public userId:String;
+    public role:string;
 
     constructor(
         private breadCrumbService: BreadcrumbService,
+        private authService:AuthService
     ) {
     }
 
     ngOnInit() {
         this.breadCrumbService.clearPath();
         this.userId = localStorage.getItem('userId');
+        this.authService.getRole().subscribe(res => this.role = res)
     }
+
+    showHomeMenu(...role) {
+        if (role.includes(this.role)) {
+          return true;
+        } else {
+          return false;
+        }
+
+        role.includes(this.role) ?true:false;
+      }
 
 }
