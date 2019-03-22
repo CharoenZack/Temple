ed55fa3bd6ca266cc18a088baa45a98e17a24d21
Sukit Pracprayoon <sukit@kkumail.com>
2019-03-22 20:33:23 +0700

    สมัคร course

--- src/app/content/courses/courses-list/courses-list.component.ts
+++ src/app/content/courses/courses-list/courses-list.component.ts
@@ -6,58 +6,60 @@ import {Course} from 'src/app/shared/interfaces/course';
 import {BreadcrumbService} from '../../../shared/service/breadcrumb.service';
 
 @Component({
-    selector: 'app-courses-list',
-    templateUrl: './courses-list.component.html',
-    styleUrls: ['./courses-list.component.css']
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent implements OnInit {
 
-    msgs: any[] = [];
-    courses: Course[];
-    cols: any[];
-    public menu: MenuItem[];
+  msgs: any[] = [];
+  courses: Course[];
+  cols: any[];
+  public menu: MenuItem[];
 
-    constructor(
-        private course: CourseService,
-        private confirmationService: ConfirmationService,
-        private breadCrumbService: BreadcrumbService,
-    ) {
-    }
+  constructor(
+    private course: CourseService,
+    private confirmationService: ConfirmationService,
+    private breadCrumbService: BreadcrumbService,
+  ) {
+  }
 
-    ngOnInit() {
-        this.course.getCourses().subscribe(res => {
-            console.log(res);
-
-            if (res['status'] === 'Success') {
-                this.courses = res['data'];
-            }
-        });
+  ngOnInit() {
+    this.course.getCourses().subscribe(res => {
+      if (res['status'] === 'Success') {
+        this.courses = res['data'];
+      }
+    });
 
 
-        this.cols = [
-            {field: 'stDate', header: 'วันที่'},
-            {field: 'name', header: 'ชื่อคอร์ส'},
-            {field: 'locationName', header: 'สถานที่'},
-            {field: 'conditionMin', header: 'หมายเหตุ'},
-        ];
+    this.cols = [
+      {field: 'stDate', header: 'วันที่'},
+      {field: 'name', header: 'ชื่อคอร์ส'},
+      {field: 'locationName', header: 'สถานที่'},
+      {field: 'conditionMin', header: 'หมายเหตุ'},
+    ];
 
-        this.breadCrumbService.setPath([
-            {label: 'Courses : ข้อมูลคอร์สทั้งหมด', routerLink: '/courses'},
-        ]);
-    }
+    this.breadCrumbService.setPath([
+      {label: 'Courses : ข้อมูลคอร์สทั้งหมด', routerLink: '/courses'},
+    ]);
+  }
 
-    confirm1() {
-        this.confirmationService.confirm({
-            message: 'Are you sure that you want to proceed?',
-            header: 'Confirmation',
-            icon: 'pi pi-exclamation-triangle',
-            accept: () => {
-                this.msgs = [{severity: 'info', summary: 'Confirmed', detail: 'You have accepted'}];
-            },
-            reject: () => {
-                this.msgs = [{severity: 'info', summary: 'Rejected', detail: 'You have rejected'}];
-            }
-        });
-    }
+  confirm1() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.msgs = [{severity: 'info', summary: 'Confirmed', detail: 'You have accepted'}];
+      },
+      reject: () => {
+        this.msgs = [{severity: 'info', summary: 'Rejected', detail: 'You have rejected'}];
+      }
+    });
+  }
+
+  public registerCourse(id: number) {
+    console.log(id);
+  }
 
 }
--- src/app/content/schedule/schedule.component.ts
+++ src/app/content/schedule/schedule.component.ts
@@ -1,118 +1,81 @@
-import { Component, OnInit } from '@angular/core';
-import { ScheduleService } from 'src/app/shared/service/schedule.service';
+import {Component, OnInit} from '@angular/core';
+import {ScheduleService} from 'src/app/shared/service/schedule.service';
+import {BreadcrumbService} from '../../shared/service/breadcrumb.service';
 
 @Component({
-  selector: 'app-schedule',
-  templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+    selector: 'app-schedule',
+    templateUrl: './schedule.component.html',
+    styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
-  events: any[];
-  options: any;
-  constructor(
-    private scheduleService: ScheduleService
-  ) {
+    events: any[];
+    options: any;
 
-  }
+    constructor(
+        private scheduleService: ScheduleService,
+        private breadCrumbService: BreadcrumbService,
+    ) {
 
-  ngOnInit() {
-    // this.events = [
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-01"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-04"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-06"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-08"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-11"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-13"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-15"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-18"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-20"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-22"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-25"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-02-27"
-    //   },
-    //   {
-    //     "title": "คอร์สธรรมะ 1",
-    //     "start": "2019-03-01"
-    //   },
-    //];
-    this.scheduleService.getSchedule()
-      .subscribe(res => {
-        console.log(res, 'schedule');
+    }
 
-        if (res['Status'] === 'Success') {
-          this.events = res['data']
-          this.events = [
-            ...this.events,
-            {
-              "title": "คอร์สธรรมะ 1",
-              "start": "2019-03-01"
-            }
-          ]
-    
-      } else {
+    ngOnInit() {
+        this.setPath();
+        this.loadData();
+        this.setOption();
+    }
 
-        }
-      },
-        err => {
-          console.log('Error', err)
-        })
+    private setPath() {
+        this.breadCrumbService.setPath([
+            {label: 'Schedule : ตารางเรียน'}
+        ]);
+    }
 
+    private loadData() {
+        this.scheduleService.getSchedule()
+            .subscribe(res => {
+                    console.log(res, 'schedule');
 
-    this.options = {
-      header: {
-        left: 'prev,today,next',
-        center: 'title',
-        right: 'month,agendaWeek'
-      },
-      locale: 'th',
-      buttonText: {
-        prev: "ย้อน",
-        next: "ถัดไป",
-        today: "วันนี้",
-        month: "เดือน",
-        week: "สัปดาห์",
-        day: "วัน",
-        list: "แผนงาน"
-      },
-      allDayText: "ตลอดวัน",
-      eventLimitText: "เพิ่มเติม",
-      noEventsMessage: "ไม่มีกิจกรรมที่จะแสดง"
-    };
-  }
+                    if (res['status'] === 'Success') {
 
+                        this.events = res['data'];
+                        this.events = [
+                            ...this.events,
+                            {
+                                'title': 'คอร์สธรรมะ 1',
+                                'start': '2019-03-01'
+                            }
+                        ];
+                        console.log(this.events);
+
+                    } else {
+
+                    }
+                },
+                err => {
+                    console.log('Error', err);
+                });
+    }
+
+    private setOption() {
+        this.options = {
+            header: {
+                left: 'prev,today,next',
+                center: 'title',
+                right: 'month,agendaWeek'
+            },
+            locale: 'th',
+            buttonText: {
+                prev: 'ย้อน',
+                next: 'ถัดไป',
+                today: 'วันนี้',
+                month: 'เดือน',
+                week: 'สัปดาห์',
+                day: 'วัน',
+                list: 'แผนงาน'
+            },
+            allDayText: 'ตลอดวัน',
+            eventLimitText: 'เพิ่มเติม',
+            noEventsMessage: 'ไม่มีกิจกรรมที่จะแสดง'
+        };
+    }
 }
