645440f68201cc15b92bb5b45cf7fd240c6d3ce3
Sukit Pracprayoon <sukit@kkumail.com>
2019-03-25 13:56:39 +0700

    edit routing

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -1,129 +1,96 @@
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CourseComponent } from './courses/course/course.component';
-import { ContentComponent } from './content.component';
-import { BaggagesComponent } from "./baggages/baggages.component";
-import { CoursesComponent } from './courses/courses.component';
-import { CoursesListComponent } from './courses/courses-list/courses-list.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { LocationComponent } from "./location/location.component";
-import { ManagedTitlenameComponent } from "./managed-titlename/managed-titlename.component";
-import { AuthGuard } from '../shared/guard/auth.guard';
-import { ManageUserComponent } from './manage-user/manage-user.component';
-import { EditFormComponent } from './edit-form/edit-form.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RegisterFormComponent } from '../auth/register-form/register-form.component';
-import { ApprovalComponent } from './approval/approval.component';
-import { ManageCourseComponent } from './manage-course/manage-course.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CourseComponent} from './courses/course/course.component';
+import {ContentComponent} from './content.component';
+import {BaggagesComponent} from './baggages/baggages.component';
+import {CoursesComponent} from './courses/courses.component';
+import {CoursesListComponent} from './courses/courses-list/courses-list.component';
+import {ScheduleComponent} from './schedule/schedule.component';
+import {LocationComponent} from './location/location.component';
+import {ManagedTitlenameComponent} from './managed-titlename/managed-titlename.component';
+import {AuthGuard} from '../shared/guard/auth.guard';
+import {ManageUserComponent} from './manage-user/manage-user.component';
+import {EditFormComponent} from './edit-form/edit-form.component';
+import {HomeComponent} from './home/home.component';
+import {ProfileComponent} from './profile/profile.component';
+import {RegisterFormComponent} from '../auth/register-form/register-form.component';
+import {ApprovalComponent} from './approval/approval.component';
+import {ManageCourseComponent} from './manage-course/manage-course.component';
 
 
 const routes: Routes = [
-    {
+  {
+    path: '',
+    component: ContentComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
         path: '',
-        component: ContentComponent,
-        canActivate: [AuthGuard],
+        component: HomeComponent,
+      },
+      {
+        path: 'courses',
+        component: CoursesComponent,
         children: [
-            {
-                path: '',
-                component: HomeComponent,
-            },
-            {
-                path: 'courses',
-                component: CoursesComponent,
-                children: [
-                    {
-                        path: '',
-                        component: CoursesListComponent,
-                    },
-                    {
-                        path: ':id',
-                        component: CourseComponent,
-                    }
-                ]
-            },
-            {
-                path: 'baggages',
-                component: BaggagesComponent,
-            },
-            {
-                path: 'profile/:id',
-                component: ProfileComponent,
-            },
-            {
-                path: 'profile/:id/edit',
-                component: EditFormComponent,
-                data: {urlback: '/profile/'}
-            },
-            {
-                path: 'schedule',
-                component: ScheduleComponent,
-            },
-            {
-                path: 'location',
-                component: LocationComponent,
-            },
-            {
-                path: 'managedTitlename',
-                component: ManagedTitlenameComponent,
-            },
-            {
-                path: 'users',
-                component: ManageUserComponent,
-            },
-            {
-                path: 'user/create',
-                component: RegisterFormComponent,
-            }
-
+          {
+            path: '',
+            component: CoursesListComponent,
+          },
+          {
+            path: ':id',
+            component: CourseComponent,
+          }
         ]
       },
       {
-        path: "baggages",
+        path: 'baggages',
         component: BaggagesComponent,
       },
       {
-        path: "profile/:id",
+        path: 'profile/:id',
         component: ProfileComponent,
       },
       {
-        path: "profile/:id/edit",
+        path: 'profile/:id/edit',
         component: EditFormComponent,
-        data: { urlback:'/profile/'}
+        data: {urlback: '/profile/'}
       },
       {
-        path: "schedule",
+        path: 'schedule',
         component: ScheduleComponent,
       },
       {
-        path: "location",
+        path: 'location',
         component: LocationComponent,
       },
       {
-        path: "managedTitlename",
+        path: 'managedTitlename',
         component: ManagedTitlenameComponent,
       },
       {
-        path: "users",
+        path: 'users',
         component: ManageUserComponent,
       },
       {
-        path: "user/create",
+        path: 'user/create',
         component: RegisterFormComponent,
       },
       {
-        path: "approval",
+        path: 'approval',
         component: ApprovalComponent,
       },
       {
-        path: "managedCourse",
-        component:ManageCourseComponent
-      }
+        path: 'manageCourse',
+        component: ManageCourseComponent
+      },
+
+    ]
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class ContentRoutingModule {
 }
