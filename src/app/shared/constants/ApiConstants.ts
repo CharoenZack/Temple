033b10f4c7625dda09cbd3ccccd7354764478d08
YO <panumas.makaew@gmail.com>
2019-04-02 13:19:35 +0700

    สู้ๆนะ

--- src/app/shared/constants/ApiConstants.ts
+++ src/app/shared/constants/ApiConstants.ts
@@ -1,6 +1,5 @@
 export class ApiConstants {
     public static get baseURl(): string {
-        return 'http://157.179.132.181:3999/api/v1';
-        // return 'http://localhost:3999/api/v1';
+        return 'http://localhost:3999/api/v1';
     }
 }
