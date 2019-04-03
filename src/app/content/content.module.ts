fc9910ab1cdcce7a461fc1715041f35c518e3e2a
Sukit Pracprayoon <sukit@kkumail.com>
2019-04-03 09:15:36 +0700

    พระอนุมัติพิเศษ

--- src/app/content/content.module.ts
+++ src/app/content/content.module.ts
@@ -21,50 +21,54 @@ import {EditUserComponent} from './manageUser/edit-user/edit-user.component';
 import {EditFormComponent} from './edit-form/edit-form.component';
 import {HomeComponent} from './home/home.component';
 import {ApprovalComponent} from './approval/approval.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ManageCourseComponent } from './manage-course/manage-course.component';
-import { ManagePassCourseComponent } from './manage-pass-course/manage-pass-course.component';
-import { ManagePassCourseFormComponent } from './manage-pass-course-form/manage-pass-course-form.component';
+import {ProfileComponent} from './profile/profile.component';
+import {ManageCourseComponent} from './manage-course/manage-course.component';
+import {ManagePassCourseComponent} from './manage-pass-course/manage-pass-course.component';
+import {ManagePassCourseFormComponent} from './manage-pass-course-form/manage-pass-course-form.component';
 import {ManageUserService} from '../shared/service/manage-user.service';
+import { ListCourseApproveComponent } from './approval/list-course-approve/list-course-approve.component';
+import {HttpClientService} from '../shared/service/http-client.service';
 
 @NgModule({
-    declarations: [
-        ContentComponent,
-        BaggagesComponent,
-        CoursesComponent,
-        CourseResgisterComponent,
-        CourseFormComponent,
-        CourseEditComponent,
-        CourseCreateComponent,
-        CoursesListComponent,
-        CourseComponent,
-        ScheduleComponent,
-        LocationComponent,
-        ManagedTitlenameComponent,
-        ManageUserComponent,
-        EditUserComponent,
-        EditFormComponent,
-        HomeComponent,
-        ApprovalComponent,
-        ProfileComponent,
-        ManageCourseComponent,
-        ManagePassCourseComponent,
-        ManagePassCourseFormComponent,
+  declarations: [
+    ContentComponent,
+    BaggagesComponent,
+    CoursesComponent,
+    CourseResgisterComponent,
+    CourseFormComponent,
+    CourseEditComponent,
+    CourseCreateComponent,
+    CoursesListComponent,
+    CourseComponent,
+    ScheduleComponent,
+    LocationComponent,
+    ManagedTitlenameComponent,
+    ManageUserComponent,
+    EditUserComponent,
+    EditFormComponent,
+    HomeComponent,
+    ApprovalComponent,
+    ProfileComponent,
+    ManageCourseComponent,
+    ManagePassCourseComponent,
+    ManagePassCourseFormComponent,
+    ListCourseApproveComponent,
 
-    ],
-    imports: [
-        CoreModule,
-        SharedModule,
-        ContentRoutingModule
-    ],
-    exports: [
-        ContentComponent
-    ],
-    providers: [
-        ConfirmationService,
-        AuthGuard,
-        ManageUserService,
-    ]
+  ],
+  imports: [
+    CoreModule,
+    SharedModule,
+    ContentRoutingModule
+  ],
+  exports: [
+    ContentComponent
+  ],
+  providers: [
+    ConfirmationService,
+    AuthGuard,
+    ManageUserService,
+    HttpClientService
+  ]
 })
 export class ContentModule {
 }
