abe677b0170cca6011704acac99d9fbf70a0da77
Sukit Pracprayoon <sukit@kkumail.com>
2019-04-05 14:13:37 +0700

    โยทำพังรอบที่ ล้าน

--- src/app/content/courses/shared/course.service.ts
+++ src/app/content/courses/shared/course.service.ts
@@ -4,26 +4,25 @@ import {map} from 'rxjs/operators';
 import {ApiConstants} from 'src/app/shared/constants/ApiConstants';
 import {SpecialApprove} from '../../../shared/interfaces/special-approve';
 import {Course} from '../../../shared/interfaces/course';
-import { HttpClientService } from 'src/app/shared/service/http-client.service';
+import {HttpClientService} from 'src/app/shared/service/http-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  private course: Course;
-
   constructor(
     private http: HttpClientService
   ) {
   }
 
-  setCourse(course: Course) {
-    this.course = course;
-  }
-
-  getCourse(): Course {
-    return this.course;
+  getCourseฺByid(id) {
+    return this.http.get(ApiConstants.baseURl + `/courses/${id}`).pipe(
+      map(res => ({
+          status: res['result'],
+          data: res['data'][0]
+        })
+      ));
   }
 
   getCourses() {
@@ -43,7 +42,7 @@ export class CourseService {
 
 
   createCourse(data) {
-    return this.http.post(ApiConstants.baseURl + `/courses`, data );
+    return this.http.post(ApiConstants.baseURl + `/courses`, data);
   }
 
   editCourse(id) {
@@ -61,7 +60,7 @@ export class CourseService {
   cancelApprovalCourse(id) {
     return this.http.delete(ApiConstants.baseURl + `/approve/${id}`);
   }
-  
+
   getTeachers() {
     return this.http.get(ApiConstants.baseURl + `/members/monk`).pipe(
       map(res => {
--- src/app/content/schedule/schedule.component.ts
+++ src/app/content/schedule/schedule.component.ts
@@ -3,79 +3,66 @@ import {ScheduleService} from 'src/app/shared/service/schedule.service';
 import {BreadcrumbService} from '../../shared/service/breadcrumb.service';
 
 @Component({
-    selector: 'app-schedule',
-    templateUrl: './schedule.component.html',
-    styleUrls: ['./schedule.component.css']
+  selector: 'app-schedule',
+  templateUrl: './schedule.component.html',
+  styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
-    events: any[];
-    options: any;
+  events: any[];
+  options: any;
 
-    constructor(
-        private scheduleService: ScheduleService,
-        private breadCrumbService: BreadcrumbService,
-    ) {
+  constructor(
+    private scheduleService: ScheduleService,
+    private breadCrumbService: BreadcrumbService,
+  ) {
 
-    }
+  }
 
-    ngOnInit() {
-        this.setPath();
-        this.loadData();
-        this.setOption();
-    }
+  ngOnInit() {
+    this.setPath();
+    this.loadData();
+    this.setOption();
+  }
 
-    private setPath() {
-        this.breadCrumbService.setPath([
-            {label: 'Schedule : ตารางเรียน'}
-        ]);
-    }
+  private setPath() {
+    this.breadCrumbService.setPath([
+      {label: 'Schedule : ตารางเรียน'}
+    ]);
+  }
 
-    private loadData() {
-        this.scheduleService.getSchedule()
-            .subscribe(res => {
-                    console.log(res, 'schedule');
+  private loadData() {
+    this.scheduleService.getSchedule()
+      .subscribe(res => {
+          console.log(res);
+          if (res['status'] === 'Success') {
+            this.events = res['data'];
+          }
+        },
+        err => {
+          console.log('Error', err);
+        });
+  }
 
-                    if (res['status'] === 'Success') {
-
-                        this.events = res['data'];
-                        this.events = [
-                            ...this.events,
-                            {
-                                'title': 'คอร์สธรรมะ 1',
-                                'start': '2019-03-01'
-                            }
-                        ];
-                        console.log(this.events);
-
-                    } else {
-
-                    }
-                },
-                err => {
-                    console.log('Error', err);
-                });
-    }
-
-    private setOption() {
-        this.options = {
-            header: {
-                left: 'prev,today,next',
-                center: 'title',
-                right: 'month,agendaWeek'
-            },
-            locale: 'th',
-            buttonText: {
-                prev: 'ย้อน',
-                next: 'ถัดไป',
-                today: 'วันนี้',
-                month: 'เดือน',
-                week: 'สัปดาห์',
-                day: 'วัน',
-                list: 'แผนงาน'
-            },
-            allDayText: 'ตลอดวัน',
-            eventLimitText: 'เพิ่มเติม',
-            noEventsMessage: 'ไม่มีกิจกรรมที่จะแสดง'
-        };
-    }
+  private setOption() {
+    this.options = {
+      header: {
+        left: 'prev,today,next',
+        center: 'title',
+        right: 'month,agendaWeek'
+      },
+      locale: 'th',
+      buttonText: {
+        prev: 'ย้อน',
+        next: 'ถัดไป',
+        today: 'วันนี้',
+        month: 'เดือน',
+        week: 'สัปดาห์',
+        day: 'วัน',
+        list: 'แผนงาน'
+      },
+      allDayText: 'ตลอดวัน',
+      eventLimitText: 'เพิ่มเติม',
+      noEventsMessage: 'ไม่มีกิจกรรมที่จะแสดง'
+    };
+  }
 }
