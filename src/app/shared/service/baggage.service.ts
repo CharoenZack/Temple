56f1df18c3627d041f1777cc6b2ea0f7f516e43f
Babybew <waroon1515@gmail.com>
2019-04-25 13:44:37 +0700

    โยทำพัง

--- src/app/shared/service/baggage.service.ts
+++ src/app/shared/service/baggage.service.ts
@@ -15,7 +15,7 @@ export class BaggageService {
   ) { }
 
   getItems() {
-    return this.http.get(ApiConstants.baseURl + '/baggages', {
+    return this.http.get(ApiConstants.baseURl + '/manage_baggages', {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('access-token')}`
       }
